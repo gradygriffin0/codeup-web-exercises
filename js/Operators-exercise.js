897ff8b87c4a9be975ec33b7e9d48514b525c27e
gradygriffin0 <grady.griffin0@gmail.com>
2021-04-15 15:26:36 -0500

    Exercises On Thursday April 15th 2021 3:26PM

---
+++ js/Operators-exercise.js
@@ -0,0 +1,92 @@
+// OPERATORS
+// PREDICTION THEN CONSOLE LOGGING
+
+// '1' + 2
+// I predict 1
+console.log('1' + 2);
+// returns "12" ---
+
+// typeof '1' + 2
+// I predict string
+console.log(typeof('1' + 2));
+// returns "string"
+
+// 6 % 4
+// I predict 2
+console.log(6 % 4);
+// returns 2
+
+// '3 + 4 is ' + 3 + 4
+// I predict "3 + 4 is 34"
+console.log('3 + 4 is ' + 3 + 4);
+// returns "3 + 4 is 34"  ---- if you wanted to get the correct value you coule put parenthesis around 3 + 4.
+
+// 0 < 0
+// I predict 0
+console.log(0 < 0);
+// returns false -- which is technically 0 but I am surprised that it returned false.
+
+// 'false' == false
+// I predict true
+console.log('false' == false);
+// returns false -- probably because false actually equals a value 0 and false does not.
+
+// true == 'true'
+// I predict false
+console.log(true == 'true');
+// returns false
+
+// 5 >= -5
+// I predict true
+console.log(5 >= -5);
+// returns true
+
+// !false || false
+// I predict true
+console.log(!false || false);
+// returns true
+
+// true || "42"
+// I predict true
+console.log(true || "42");
+// returns true
+
+// !true && !false
+// I predict false
+console.log(!true && !false);
+// returns false
+
+// 6 % 5
+// I predict 1
+console.log(6 % 5);
+// returns 1
+
+// 5 < 4 && 1 === 1
+// I predict false
+console.log(5 < 4 && 1 === 1);
+// returns false
+
+// typeof 'codeup' === 'string'
+// I predict true.
+console.log(typeof 'codeup' === 'string');
+// returns true
+
+// 'codeup' === 'codeup' && 'codeup' === 'Codeup'
+// I predict false
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup');
+// returns false
+
+// 4 >= 0 && 1 !== '1'
+// I predict true
+console.log(4 >= 0 && 1 !== '1');
+// returns true
+
+// 6 % 3 === 0
+// I predict true
+console.log(6 % 3 === 0);
+// returns true
+
+// 5 % 2 !== 0
+// I predict true
+console.log(5 % 2 !== 0);
+// returns true
---
+++ js/primitive-types-exercises.js
@@ -0,0 +1,20 @@
+/* -- Questions
+TODO: What data type would best represent:
+*/
+// A term or phrase typed into a search box?
+// String
+
+// If a user is logged in?
+// Boolean.
+
+// A discount amount to apply to a user's shopping cart?
+// Number
+
+// Whether or not a coupon code is valid?
+// Boolean
+
+// An email address typed into a registration form?
+// String
+
+// The price of a product?
+// Number
