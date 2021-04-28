3f201759e2e019a755ba29bdf8b09ba00077c542
gradygriffin0 <grady.griffin0@gmail.com>
2021-04-28 08:48:48 -0500

    earlier exercise correction

---
+++ js/break_and_continue.js
@@ -0,0 +1,20 @@
+// do{
+// var userNumber = (prompt("Enter an odd number between 0 and 50"));
+// userNumber = Number(userNumber);
+// if ((userNumber % 2 === 1) && (isNaN(userNumber) === false) && (userNumber <= 50) && (userNumber >= 0)){
+//     break;
+// }
+//
+// } while(1 === 1);
+//
+// console.log(userNumber);
+//
+// for (let i = 0; i <= 50; i++){
+//     if(i === userNumber){
+//         console.log(`${i} is your favorite number? skipped!`);
+//         continue;
+//     }
+//     else if (i % 2 === 1){
+//         console.log(`Odd Number is ${i}`)
+//         continue;
+//     }
---
+++ js/for-loops.js
@@ -0,0 +1,90 @@
+// For Exercises
+//         let showMultiplicationTable = function(number){
+//              for(let i = 0; i <= 10; i++){
+//                 console.log(`${number} x ${i} = ${number*i}`);
+//             }
+//         }
+//         showMultiplicationTable(7);
+
+// let tenRandomCheck = function(){
+//     for (let i = 1; i<= 10; i++){
+//         var number = Math.floor(Math.random() * 180 + 1) + 20;
+//         if (number % 2 === 0){
+//             console.log(`${i}: ${number} is even`);
+//         } else {
+//             console.log(`${i}: ${number} is odd`);
+//         }
+//
+//     }
+// }
+// tenRandomCheck();
+//
+//     let numberPyramid = function(start, end){
+//         for(let i = start; i <= end; i++) {
+//             var x = String(i);
+//             x = x.repeat(i);
+//             console.log(x);
+//         }
+//
+//     }
+//     numberPyramid(0,9);
+
+// for (let i = 100; i>0 ; i-=5)
+//     console.log(i);
+
+
+// }// For Exercises
+// //         let showMultiplicationTable = function(number){
+// //              for(let i = 0; i <= 10; i++){
+// //                 console.log(`${number} x ${i} = ${number*i}`);
+// //             }
+// //         }
+// //         showMultiplicationTable(7);
+//
+// // let tenRandomCheck = function(){
+// //     for (let i = 1; i<= 10; i++){
+// //         var number = Math.floor(Math.random() * 180 + 1) + 20;
+// //         if (number % 2 === 0){
+// //             console.log(`${i}: ${number} is even`);
+// //         } else {
+// //             console.log(`${i}: ${number} is odd`);
+// //         }
+// //
+// //     }
+// // }
+// // tenRandomCheck();
+// //
+// //     let numberPyramid = function(start, end){
+// //         for(let i = start; i <= end; i++) {
+// //             var x = String(i);
+// //             x = x.repeat(i);
+// //             console.log(x);
+// //         }
+// //
+// //     }
+// //     numberPyramid(0,9);
+//
+// // for (let i = 100; i>0 ; i-=5)
+// //     console.log(i);
+//
+// // do{
+// // var userNumber = (prompt("Enter an odd number between 0 and 50"));
+// // userNumber = Number(userNumber);
+// // if ((userNumber % 2 === 1) && (isNaN(userNumber) === false) && (userNumber <= 50) && (userNumber >= 0)){
+// //     break;
+// // }
+// //
+// // } while(1 === 1);
+// //
+// // console.log(userNumber);
+// //
+// // for (let i = 0; i <= 50; i++){
+// //     if(i === userNumber){
+// //         console.log(`${i} is your favorite number? skipped!`);
+// //         continue;
+// //     }
+// //     else if (i % 2 === 1){
+// //         console.log(`Odd Number is ${i}`)
+// //         continue;
+// //     }
+// // }
