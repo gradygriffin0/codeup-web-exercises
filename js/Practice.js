423a396229e0237352479e95ffbfed62d82d7dcb
gradygriffin0 <grady.griffin0@gmail.com>
2021-04-30 19:59:55 -0500

    Added practice exercise javascript file. Fri Apr 30 7:59PM

---
+++ js/Practice.js
@@ -0,0 +1,11 @@
+"use strict";
+
+function lessThanOrEqualToZero(number){
+    if (number <= 0){
+        return true;
+    } else {
+        return false;
+    }
+}
+
+console.log(lessThanOrEqualToZero(-420));;
