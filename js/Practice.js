1aed836dc34bb5b9125b206a04688276cb9902e4
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-01 09:09:12 -0500

    Worked on Edabit, May 1 2021 9:08AM

--- js/Practice.js
+++ js/Practice.js
@@ -9,3 +9,15 @@ function lessThanOrEqualToZero(number){
 }
 
 console.log(lessThanOrEqualToZero(-420));;
+
+function minMax(arr){
+    let newArr = [];
+    newArr.unshift(Math.max.apply(null, arr));
+    newArr.unshift(Math.min.apply(null, arr));
+    return newArr;
+
+}
+
+console.log(minMax([5,4,3,2,1]));
+
+console.log(Math.min([5,4,3,2,1]));
