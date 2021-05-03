a58dd39a1985a96c879dcac2f4cadc407286aa03
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-03 09:28:38 -0500

    Warmup Mon May 3 2021 9:28AM

--- js/Practice.js
+++ js/Practice.js
@@ -1,23 +1,36 @@
 "use strict";
 
-function lessThanOrEqualToZero(number){
-    if (number <= 0){
-        return true;
-    } else {
-        return false;
+// function lessThanOrEqualToZero(number){
+//     if (number <= 0){
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+//
+// console.log(lessThanOrEqualToZero(-420));;
+//
+// function minMax(arr){
+//     let newArr = [];
+//     newArr.unshift(Math.max.apply(null, arr));
+//     newArr.unshift(Math.min.apply(null, arr));
+//     return newArr;
+//
+// }
+//
+// console.log(minMax([5,4,3,2,1]));
+//
+// console.log(Math.min([5,4,3,2,1]))
+
+function trim(input){
+    if (typeof input === "string")
+        return input.trim();
+    else {
+        return "Error";
     }
-}
-
-console.log(lessThanOrEqualToZero(-420));;
-
-function minMax(arr){
-    let newArr = [];
-    newArr.unshift(Math.max.apply(null, arr));
-    newArr.unshift(Math.min.apply(null, arr));
-    return newArr;
 
 }
 
-console.log(minMax([5,4,3,2,1]));
-
-console.log(Math.min([5,4,3,2,1]));
+console.log(trim(3));
+console.log(trim(" asd asdf "));
+console.log(trim("             a              b             "))
