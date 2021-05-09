21d27a6a78d8cd98ff4b8ceaa752aa2293831b3a
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-09 13:21:23 -0500

    Coding Prac.

--- js/practice.js
+++ js/practice.js
@@ -5,24 +5,40 @@
 })();
 
 
-function simpleArraySum(input) {
-    let sum = 0;
-    for (let i = 0; i < input.length; i++) {
-        sum += input[i];
-    }
-    return sum;
-}
+// function simpleArraySum(input) {
+//     let sum = 0;
+//     for (let i = 0; i < input.length; i++) {
+//         sum += input[i];
+//     }
+//     return sum;
+// }
+//
+// function compareTriplets(a, b) {
+//     let compareAB = [0, 0];
+//     for (let i = 0; i < a.length; i++) {
+//         if (a[i] > b[i]) {
+//             ++compareAB[0]
+//         } else if (a[i] < b[i]) {
+//             ++compareAB[1]
+//         }
+//     }
+//     return compareAB;
+// }
+//
+// console.log(compareTriplets([17, 28, 30], [99, 16, 8]));
+//
+//
+// function aVeryBigSum(ar) {
+//     let sum = 0;
+//     for( let i = 0; i < ar.length; i++) {
+//         sum += ar[i];
+//     }
+//     return (sum);
+// }
+//
+// aVeryBigSum([1000000001, 1000000002, 1000000003, 1000000004, 1000000005]);
 
-function compareTriplets(a, b) {
-    let compareAB = [0, 0];
-    for (let i = 0; i < a.length; i++) {
-        if (a[i] > b[i]) {
-            ++compareAB[0]
-        } else if (a[i] < b[i]) {
-            ++compareAB[1]
-        }
-    }
-    return compareAB;
-}
+function minTime(files, numCores, limit) {
 
-console.log(compareTriplets([17, 28, 30], [99, 16, 8]));
+
+}
