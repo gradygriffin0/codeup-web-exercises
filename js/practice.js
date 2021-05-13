7bea2d9be353e7dbc81133990e78ffbae550bcde
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-13 09:35:23 -0500

    Warmup May 13 2021

--- js/practice.js
+++ js/practice.js
@@ -77,25 +77,40 @@
 // minTime([2, 4, 8, 16, 16], 4, 2);
 //
 // console.log(minTime([2, 4, 8, 16], 4, 1));
-let test = [11, 2, 4, 4, 5, 6, 10, 8, -12];
-function diagonalDifference(arr) {
-    let n = (Math.pow(arr.length, 1/2));
+// let test = [11, 2, 4, 4, 5, 6, 10, 8, -12];
+// function diagonalDifference(arr) {
+//     let n = (Math.pow(arr.length, 1/2));
+//
+//     let topLeft = arr[0];
+//     let topRight = arr[n-1];
+//     if (arr.length % 2 === 0){
+//         var middle = arr[Math.floor(arr.length / 2)];
+//     } else {
+//         var middle = arr[Math.floor((arr.length / 2) - 1)];
+//     }
+//     let bottomLeft = arr[(n*2)];
+//     let bottomRight = arr[arr.length - 1];
+//
+//     let sum1 = topLeft + middle + bottomRight;
+//     let sum2 = topRight + middle + bottomLeft;
+//
+//     let difference = Math.abs((sum1 - sum2));
+//     console.log(difference);
+//     return difference;
+// }
+// diagonalDifference(test);
 
-    let topLeft = arr[0];
-    let topRight = arr[n-1];
-    if (arr.length % 2 === 0){
-        var middle = arr[Math.floor(arr.length / 2)];
-    } else {
-        var middle = arr[Math.floor((arr.length / 2) - 1)];
+// May 13
+//  Write a JavaScript program to construct the following pattern, using a nested for loop.
+// *
+// * *
+// * * *
+// * * * *
+// * * * * *
+
+ function pyramidNumber(num) {
+    var number = '';
+    for (let i = 0; i <= num; i++){
+        console.log(number += i)
     }
-    let bottomLeft = arr[(n*2)];
-    let bottomRight = arr[arr.length - 1];
-
-    let sum1 = topLeft + middle + bottomRight;
-    let sum2 = topRight + middle + bottomLeft;
-
-    let difference = Math.abs((sum1 - sum2));
-    console.log(difference);
-    return difference;
 }
-diagonalDifference(test);
