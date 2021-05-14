5400bd95abc86e35a1b0847846034881ea803f53
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-14 08:17:11 -0500

    First tab complete pizza exercise

---
+++ js/order-pizza-snippet.js
@@ -0,0 +1,38 @@
+// function bootstrapTabControl(){
+//     var i, items = $('.nav-link'), pane = $('.tab-pane');
+//     // next
+//     $('.nexttab').on('click', function(){
+//         for(i = 0; i < items.length; i++){
+//             if($(items[i]).hasClass('active') == true){
+//                 break;
+//             }
+//         }
+//         if(i < items.length - 1){
+//             // for tab
+//             $(items[i]).removeClass('active');
+//             $(items[i+1]).addClass('active');
+//             // for pane
+//             $(pane[i]).removeClass('show active');
+//             $(pane[i+1]).addClass('show active');
+//         }
+//
+//     });
+//     // Prev
+//     $('.prevtab').on('click', function(){
+//         for(i = 0; i < items.length; i++){
+//             if($(items[i]).hasClass('active') == true){
+//                 break;
+//             }
+//         }
+//         if(i != 0){
+//             // for tab
+//             $(items[i]).removeClass('active');
+//             $(items[i-1]).addClass('active');
+//             // for pane
+//             $(pane[i]).removeClass('show active');
+//             $(pane[i-1]).addClass('show active');
+//         }
+//     });
+// }
+// bootstrapTabControl();
+
