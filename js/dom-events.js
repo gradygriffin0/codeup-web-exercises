efd8f458e3d8ea5d924e14f2b8da6dfc8961a90a
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-22 11:24:06 -0500

    fully working blog functions

--- js/dom-events.js
+++ js/dom-events.js
@@ -1,5 +1,3 @@
-
-
 addButtonEvent();
 
 function addButtonEvent() {
@@ -14,9 +12,32 @@ function addPost() {
 
 }
 
-function buildBlogPost(){
+function buildBlogPost() {
+    let container = document.createElement('div');
+    container.setAttribute('class', `col-12 border mb-1`);
+    container.appendChild(getDateNode());
+    container.appendChild(getBlogText());
+    container.appendChild(getNameNode());
+    return container;
+
+}
+
+function getBlogText() {
     let userText = document.getElementById('user-input');
     let newPost = document.createElement('p');
     newPost.textContent = userText.value;
     return newPost;
+}
+
+function getDateNode() {
+    let date = document.createElement('p');
+    date.textContent = new Date(Date.now()).toDateString();
+    return date;
+}
+
+function getNameNode() {
+    let userNameInput = document.getElementById('username');
+    let userName = document.createElement('p');
+    userName.textContent = `Submitted by  ${userNameInput.value}`;
+    return userName;
 }
