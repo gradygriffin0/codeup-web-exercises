e109e915d106a89a95dfafe22f3f4f64dcaba047
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-22 10:59:34 -0500

    IDKWTF

--- js/dom-events.js
+++ js/dom-events.js
@@ -1,14 +1,21 @@
+
+
+
 function addButtonEvent() {
     document.getElementById("submitBtn")
         .addEventListener("click", addPost)
 }
 
 function addPost() {
-    let userText = document.getElementById('user-input');
-    let newPost = document.createElement(buildBlogPost);
+
+    let container = document.getElementById('blog-container');
+    container.appendChild(buildBlogPost());
 
 }
 
 function buildBlogPost(){
-    let newPost = document.createElement()
+    let userText = document.getElementById('user-input');
+    let newPost = document.createElement('div');
+    newPost.textContent = userText.value;
+    return newPost;
 }
