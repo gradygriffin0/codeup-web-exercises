e3ebf5aa9e9b2620c369423843d0307b9673dcd8
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-24 17:37:58 -0500

    self prac

--- js/searchbar.js
+++ js/searchbar.js
@@ -1,28 +1,28 @@
 "use strict";
 
-function renderPage() {
-    document.innerHTML = `<div class="container-fluid col-12 bg-light p-4 m-4 w-auto">
-            <form class="d-flex">
-                <input id="mySearch" onkeyup="searchFunction()" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-                <button class="btn btn-outline-success" type="submit">Search</button>
-            </form>
-
-            <div class="container-fluid col-12 bg-light p-4 m-4 w-auto">
-                <ul id="myUL" class="list-group">
-                    <li class="list-group-item"><a href="#">Adele</a></li>
-                    <li class="list-group-item"><a href="#">Agnes</a></li>
-
-                    <li class="list-group-item"><a href="#">Billy</a></li>
-                    <li class="list-group-item"><a href="#">Bob</a></li>
-
-                    <li class="list-group-item"><a href="#">Calvin</a></li>
-                    <li class="list-group-item"><a href="#">Christina</a></li>
-                    <li class="list-group-item"><a href="#">Cindy</a></li>
-                </ul>
-            </div>
-
-        </div>`
-}
+// function renderPage() {
+//     document.innerHTML = `<div class="container-fluid col-12 bg-light p-4 m-4 w-auto">
+//             <form class="d-flex">
+//                 <input id="mySearch" onkeyup="searchFunction()" class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+//                 <button class="btn btn-outline-success" type="submit">Search</button>
+//             </form>
+//
+//             <div class="container-fluid col-12 bg-light p-4 m-4 w-auto">
+//                 <ul id="myUL" class="list-group">
+//                     <li class="list-group-item"><a href="#">Adele</a></li>
+//                     <li class="list-group-item"><a href="#">Agnes</a></li>
+//
+//                     <li class="list-group-item"><a href="#">Billy</a></li>
+//                     <li class="list-group-item"><a href="#">Bob</a></li>
+//
+//                     <li class="list-group-item"><a href="#">Calvin</a></li>
+//                     <li class="list-group-item"><a href="#">Christina</a></li>
+//                     <li class="list-group-item"><a href="#">Cindy</a></li>
+//                 </ul>
+//             </div>
+//
+//         </div>`
+// }
 
 function searchFunction() {
     var searchInput, filter, ul, li, a, textValue;
