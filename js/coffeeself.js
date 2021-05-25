6bd36d5180c9eedc74c74a3b91ca9de42c2b067c
gradygriffin0 <grady.griffin0@gmail.com>
2021-05-24 19:07:55 -0500

    working searchbar

--- js/coffeeself.js
+++ js/coffeeself.js
@@ -1,13 +1,23 @@
 "use strict"
 
-function renderCoffee(coffee) {
-    var html = '<div class="coffee">';
-    html += '<p>' + coffee.id + '</p>';
-    html += '<p>' + coffee.name + '</p>';
-    html += '<p>' + coffee.roast + '</p>';
-    html += '</div>';
+// function renderCoffee(coffee) {
+//     var html = '<div class="coffee">';
+//     html += '<p>' + coffee.id + '</p>';
+//     html += '<p>' + coffee.name + '</p>';
+//     html += '<p>' + coffee.roast + '</p>';
+//     html += '</div>';
+//
+//     return html;
+// }
+
+// changed the table into a list item of divs
+function renderCoffee(coffee){
+    var html = `<li> <div id="${coffee.id}" class="coffee">`;
+    html += `${coffee.name} ${coffee.roast}`;
+    html += '</div> </li>';
 
     return html;
+
 }
 
 function renderCoffees(coffees) {
@@ -47,6 +57,28 @@ var coffees = [
     {id: 13, name: 'Italian', roast: 'dark'},
     {id: 14, name: 'French', roast: 'dark'},
 ];
+
+function searchFunction(){
+    var searchInput, filter, ul, li, div, textValue, result;
+    searchInput = document.getElementById('mySearch');
+    filter = searchInput.value.toLowerCase();
+    ul = document.getElementById('coffees');
+    li = ul.getElementsByTagName('li');
+
+    for (let i = 0; i < li.length; i++) {
+        result = li[i].getElementsByTagName("div")[0];
+        console.log(result)
+
+        textValue = result.textContent || result.innerText;
+        if (textValue.toLowerCase().indexOf(filter) > -1) {
+            li[i].style.display = "";
+        } else {
+            li[i].style.display = "none";
+        }
+
+    }
+
+}
 
 var tbody = document.getElementById('coffees');
 var submitButton = document.querySelector('#submit');
