fda9794030456b277fa3e26e84954b95a90fd635
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-04 10:59:39 -0500

    todo duck

--- js/duck_hover_todo.js
+++ js/duck_hover_todo.js
@@ -12,27 +12,42 @@ $(document).ready(function (){
 function addListeners(){
     //get the elements to add listener and change image - check
             // let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
-    let cardContainers = $('.col-md-3').toArray();
+    let cardContainers = $('.col-md-3')
 
     addEvents(cardContainers, 'assets/img/jazz-music-rubber-duck.jpg', 'mouseover');
     addEvents(cardContainers, 'assets/img/question.png', 'mouseout');
 }
 //add mouseover/mouseout events to all selected elements
 function addEvents(cardContainers, imgPath, listenerType){
+    console.log(cardContainers);
+    console.log(imgPath);
+    console.log(listenerType);
+    console.log($('.col-md-3'));
     // cardContainers.forEach((cc) => {
     //     let card = cc;
-    //     //create listener which will call function change the image
-    //     let listener = function (event) {
-    //         let cardImg = card.querySelector(".card .card-img-top");
+    //     console.log(card)
+    //     create listener which will call function change the image
+    //     let listener = function(imgPath)  {
+    //         let cardImg = $(".card .card-img-top")
     //         changeImage(cardImg, imgPath);
     //     };
     //     cc.addEventListener(listenerType, listener);
     // });
-    // cardContainers.hover(
-    //     function
-    // )
+
+    $('.col-md-3').hover(function(){
+            console.log("testhover");
+        let cardImg = $(this);
+
+            console.log(cardImg);
+        changeImage(cardImg, 'assets/img/jazz-music-rubber-duck.jpg');
+    },
+        function (){
+            let cardImg = $(this);
+            changeImage(cardImg, 'assets/img/question.png');
+        });
+
 }
 //actual function to swap images
 function changeImage(card, imgPath){
-    card.setAttribute('src', imgPath);
+    $(card).find('img').attr('src', imgPath);
 }
