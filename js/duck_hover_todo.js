d9629523ea810196c766a7ae401e10e5cbcb090e
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-07 13:31:13 -0500

    yuh

--- js/duck_hover_todo.js
+++ js/duck_hover_todo.js
@@ -1,24 +1,25 @@
 "use strict";
 //make sure the DOM is loaded first
-$(document).ready(function (){
+$(document).ready(function () {
     addListeners();
 })
 
 //^     // document.addEventListener("DOMContentLoaded", function() {
-        //     addListeners();
-        // });
+//     addListeners();
+// });
 
 //parent method for adding all listeners -check
-function addListeners(){
+function addListeners() {
     //get the elements to add listener and change image - check
-            // let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
+    // let cardContainers = Array.from(document.querySelectorAll(".col-md-3"));
     let cardContainers = $('.col-md-3')
 
     addEvents(cardContainers, 'assets/img/jazz-music-rubber-duck.jpg', 'mouseover');
     addEvents(cardContainers, 'assets/img/question.png', 'mouseout');
 }
+
 //add mouseover/mouseout events to all selected elements
-function addEvents(cardContainers, imgPath, listenerType){
+function addEvents(cardContainers, imgPath, listenerType) {
     console.log(cardContainers);
     console.log(imgPath);
     console.log(listenerType);
@@ -34,27 +35,40 @@ function addEvents(cardContainers, imgPath, listenerType){
     //     cc.addEventListener(listenerType, listener);
     // });
 
-    $('.col-md-3').hover(function(){
+    $('.col-md-3').hover(function () {
             console.log("testhover");
-        let cardImg = $(this);
+            let cardImg = $(this);
 
             console.log(cardImg);
-        changeImage(cardImg, 'assets/img/jazz-music-rubber-duck.jpg');
-    },
-        function (){
+            changeImage(cardImg, 'assets/img/jazz-music-rubber-duck.jpg');
+        },
+        function () {
             let cardImg = $(this);
             changeImage(cardImg, 'assets/img/question.png');
         });
 
 }
+
 //actual function to swap images
-function changeImage(card, imgPath){
+function changeImage(card, imgPath) {
     $(card).find('img').attr('src', imgPath);
 }
 
+$(document).ready(function () {
+    $('.list-group-item').click(function (e) {
+        var clickedListItem = $(this);
+        clickedListItem.toggleClass('clicked');
+    })
+})
 $(document).ready(function(){
- $('.list-group-item').click(function(e){
-     var clickedListItem = $(this);
-     clickedListItem.toggleClass('clicked');
- })
+    $('#submitBtn').click(function(e){
+        let newSite = $('#redirect-url').val();
+        if (!newSite.includes('https://'){
+            window.location.assign('https://' + newSite);
+        }
+        else{
+            window.location.assign(newSite);
+        }
+
+    })
 })
