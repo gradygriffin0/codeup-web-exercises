660dd6b19eccc1c5512cabf13f75aa11a9612e08
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-08 11:23:44 -0500

    working new todo

---
+++ js/ajax_lecture.js
@@ -0,0 +1,53 @@
+(function () {
+    "use strict";
+})();
+
+// TODO: create a JSON object about your favorite hobby. make sure to include at least one of every data type that can
+//  be accepted as values
+// JSON OBJECT
+// {
+//     "name":"gaming",
+//     "yearsActive": 16,
+//     "isSkilled": true,
+//     "yearRetired": null,
+//     "favoriteGames": ["counter-strike", "league of legends", "half-life"],
+//     "favoritePlatform": {
+//         name: "steam",
+//         bestPlatform: true,
+//     }
+// }
+//AJAX
+// JQUERY REQUEST
+// $.ajax()
+
+// status property tells you if the request was successful its a number,
+// 200 is minimum for successful request
+// 300 is redirected successful
+// 400 is failed request ( err 404 )
+// 500 is server error
+
+//GET request - getting information
+// console.log($.ajax("https://jsonplaceholder.typicode.com/comments"));
+// $.ajax("https://jsonplaceholder.typicode.com/comments", {
+//     //ready to send data POST;
+//     type: "POST",
+//     // data is anything you are passing to the server;
+//     data: {
+//         title: "test",
+//         article: "test2",
+//     }
+// })
+
+//URL - location to go look for something
+
+
+//POST -adding/sending information
+
+
+//DELETE - specify information in request to be DELETED
+
+
+//PUT - specify information in request to be EDITED
+
+
+// TODO: using https://jsonplaceholder.typicode.com/ make an ajax request to the comments endpoint and console it.
