eb7f4df8682f48aea6da7a001dcf82dfc7ed8cd5
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-08 14:24:48 -0500

    both methods

--- js/ajax_lecture.js
+++ js/ajax_lecture.js
@@ -52,32 +52,58 @@
 
 // Responses
 
+// Failed .fail()
+// intentionally broken url to trigger 404;
+// $.ajax("https://jsonplaceholder.typie.com/posts", {
+//     type: "POST",
+//     data: {
+//         title: "test",
+//         body: "test2"
+//     }
+// }).fail(function(jqHxr, status, error){
+//     console.log(jqHxr); // returns object
+//     console.log(status); //returns statusText
+//     console.log(error);  // returns specific error
+//
+// });
+
+// both
+    // $.ajax("", {
+    //     type: "GET",
+    //     success: function(){
+    //         alert ("success");
+    //     },
+    //     error: function(){
+    //         alert("error");
+    //     }
+    // });
+
 //successful
 
-// $.ajax("https://jsonplaceholder.typicode.com/posts").done(function(data, status){
-//   console.log(status);
-//     console.log(data);
-//     console.log(data[0]);
-//   // alert('posted brah');
-// })
+    // $.ajax("https://jsonplaceholder.typicode.com/posts").done(function(data, status){
+    //   console.log(status);
+    //     console.log(data);
+    //     console.log(data[0]);
+    //   // alert('posted brah');
+    // })
 
 // TODO: using https://jsonplaceholder.typicode.com/ make an ajax request to the comments endpoint and console it.
 // TODO: make a POST request to the '/posts' endpoint, make sure to include any data required for that post to be made successfully
 
-    // console.log($.ajax("https://jsonplaceholder.typicode.com/comments"));
-    // console.log($.ajax("https://jsonplaceholder.typicode.com/posts", {
-    //     type: "POST",
-    //     data: {
-    //         "userId": 11,
-    //         "id": 101,
-    //         "title": "GG",
-    //         "body": "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut"
-    //     }
-    // }));
+// console.log($.ajax("https://jsonplaceholder.typicode.com/comments"));
+// console.log($.ajax("https://jsonplaceholder.typicode.com/posts", {
+//     type: "POST",
+//     data: {
+//         "userId": 11,
+//         "id": 101,
+//         "title": "GG",
+//         "body": "cupiditate quo est a modi nesciunt soluta\nipsa voluptas error itaque dicta in\nautem qui minus magnam et distinctio eum\naccusamus ratione error aut"
+//     }
+// }));
 
-    // console.log($.ajax("https://jsonplaceholder.typicode.com/posts"));
+// console.log($.ajax("https://jsonplaceholder.typicode.com/posts"));
 
-$('#clickMe').click(function(){
+$('#clickMe').click(function () {
     var userInput = $('#userName').val();
     var passInput = $('#password').val();
 
