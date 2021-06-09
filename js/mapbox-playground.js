6d61268b5f7b065287fc8e8c045a1d925117f82f
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-09 10:55:46 -0500

    free push

---
+++ js/mapbox-playground.js
@@ -0,0 +1,9 @@
+"use strict";
+
+//mapbox token;
+const token = "pk.eyJ1IjoiZ3JhZG1hbiIsImEiOiJja3Bwbjg2emIwNjVtMnFtb3NqNGxodmZoIn0.eYT2GG1ZKWBdIQDT-Z_7wQ";
+
+var map = new mapboxgl.Map({
+    container: "map",
+    style: 'mapbox://styles/mapbox',
+});
