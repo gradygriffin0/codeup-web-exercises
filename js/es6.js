0aec108d92255932a100fa01785812ad5ad4aef3
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-15 10:06:14 -0500

    working exercise

--- js/es6.js
+++ js/es6.js
@@ -33,20 +33,19 @@ const users = [
 // to the languages array
 // TODO: replace the `var` keyword with `const`, then try to reassign a variable
 // declared as `const`
-var name = 'your_name_here';
-var email = '';
-var languages = [];
+const name = 'grady';
+const email = 'grady@gmail.com';
+const languages = ["html","javascript","css"];
 
 // TODO: rewrite the object literal using object property shorthand
 users.push({
-  name: name,
-  email: email,
-  languages: languages
+  name,
+  email,
+  languages
 });
-
 // TODO: replace `var` with `let` in the following variable declarations
-var emails = [];
-var names = [];
+let emails = [];
+let names = [];
 
 // TODO: rewrite the following using arrow functions
 users.forEach(function(user) {
