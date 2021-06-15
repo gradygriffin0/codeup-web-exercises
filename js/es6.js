0399ed238f56a5a5a0a43b97f6b9e484eb6ae91f
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-15 10:11:01 -0500

    problem solved

--- js/es6.js
+++ js/es6.js
@@ -48,12 +48,8 @@ let emails = [];
 let names = [];
 
 // TODO: rewrite the following using arrow functions
-users.forEach(function(user) {
-  return emails.push(user.email);
-});
-users.forEach(function(user) {
-  return names.push(user.name);
-});
+users.forEach( (user) => emails.push(user.email));
+users.forEach((user) => names.push(user.name));
 
 // TODO: replace `var` with `let` in the following declaration
 var developers = [];
