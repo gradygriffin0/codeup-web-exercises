9b0fbb95c8631e1591b8344133f71b1eedfd4385
gradygriffin0 <grady.griffin0@gmail.com>
2021-06-15 11:20:20 -0500

    template literal exercise

--- js/es6.js
+++ js/es6.js
@@ -52,17 +52,16 @@ users.forEach( (user) => emails.push(user.email));
 users.forEach((user) => names.push(user.name));
 
 // TODO: replace `var` with `let` in the following declaration
-var developers = [];
+let developers = [];
 users.forEach(function(user) {
   // TODO: rewrite the code below to use object destructuring assignment
   //       note that you can also use destructuring assignment in the function
   //       parameter definition
-  const name = user.name;
-  const email = user.email;
-  const languages = user.languages;
+  const  {name, email, languages} = user
 
   // TODO: rewrite the assignment below to use template strings
-  developers.push(name + '\'s email is ' + email + name + ' knows ' + languages.join(', '));
+  developers.push(`${name}'s email is ${email}. ${name} knows ${langauges.join(', ')}`);
+
 });
 
 // TODO: Use `let` for the following variable
